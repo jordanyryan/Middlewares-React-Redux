fa942454b7d5773adc09b891702d8f415ffb2a4d
Jordany Rosas <jordanyryan@gmail.com>
2018-01-09 19:03:19 -0500

    Created action creator to handle static dummy data for components

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,13 @@
 import axios from 'axios';
+import {FETCH_USERS} from './types';
+
+export function fetchUsers() {
+  return {
+    type: FETCH_USERS,
+    payload: [
+      {name: 'Jane'},
+      {name: 'Alex'},
+      {name: 'Jim'}
+    ]
+  }
+}
