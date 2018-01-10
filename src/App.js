79b19f34159d12676fc7a98cd94f3776ae2b21e2
Jordany Rosas <jordanyryan@gmail.com>
2018-01-09 19:16:17 -0500

    Created UserList component to render our users information

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
-import './App.css';
+import UserList from './components/user_list';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        Welcome to React-Redux-Stater
+        <UserList />
       </div>
     );
   }
