ffc828f6184b1791e79de476811074bec8c1586f
Jordany Rosas <jordanyryan@gmail.com>
2018-01-09 20:06:06 -0500

    Built middleware that replicates functionality of redux-promise package to intercept promises and resolve them, and then dispatch them back into actions

--- src/middlewares/async.js
+++ src/middlewares/async.js
@@ -1,7 +1,8 @@
 export default function({dispatch}) {
   return next => action => {
-    console.log(action);
-
-    next(action);
-  };
+    if (!action.payload || !action.payload.then) return next(action);
+    
+    action.payload
+    .then( response => dispatch({...action, payload: response}))
+  }
 }
