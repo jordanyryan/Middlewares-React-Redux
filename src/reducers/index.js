330364f0de04a1dffacb1af918de55636c981b5a
Jordany Rosas <jordanyryan@gmail.com>
2018-01-09 19:00:20 -0500

    Setup users reducer, added case in switch, and created types file for action constants

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
 import {reducer as formReducer} from 'redux-form';
+import usersReducer from './users_reducer';
 
 
 const rootReducer = combineReducers({
-  form: formReducer
+  form: formReducer,
+  users: usersReducer
 })
 
 export default rootReducer;
